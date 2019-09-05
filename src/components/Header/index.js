c9c68f8ae7d6bfbcc10341e7a9bf5c1fb53b57e1
LucasKeiti28 <lucaskeiti28@gmail.com>
2019-09-05 13:21:49 -0300

    chore: Layout built with ReactJS.

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import GlobalStyles from "../../styles/global";
+import { Container } from "./styles";
+
+export default function Header() {
+  return (
+    <Container>
+      <h1>Pipefy</h1>
+    </Container>
+  );
+}
